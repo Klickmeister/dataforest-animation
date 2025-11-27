469b62a9fce7872fca6d1b1e943b403e9d6368ef
Finn Nils Maybauer <hi@finnmaybauer.me>
2025-11-27 11:19:41 +0100

    make selector without ids

--- src/animation-001.ts
+++ src/animation-001.ts
@@ -1,29 +1,33 @@
 import { createTimeline, utils } from "animejs";
 
+const CANVAS_SELECTOR = '[data-js-canvas="animation-001"]';
+const elementSelector = (selector: string) =>{
+  return `${CANVAS_SELECTOR} [data-js-anim-el='${selector}']`;
+}
+
 function resetSprites() {
   utils.set([ // first frame setup
-    '#canvas-art-1__left-bracket',
-    '#canvas-art-1__right-bracket',
+    elementSelector('left-bracket'),
+    elementSelector('right-bracket'),
   ], {
     opacity: 1,
     scale: 1,
   });
   utils.set([ // everything else
-    '#canvas-art-1__left-bracket-1',
-    '#canvas-art-1__right-bracket-1',
-    '#canvas-art-1__left-bracket-2',
-    '#canvas-art-1__right-bracket-2',
-    '#canvas-art-1__asterisk',
-    '#canvas-art-1__rain-part-1',
-    '#canvas-art-1__rain-part-2',
-    '#canvas-art-1__rain-part-3',
-    '#canvas-art-1__rain-part-4',
+    elementSelector('left-bracket-1'),
+    elementSelector('right-bracket-1'),
+    elementSelector('left-bracket-2'),
+    elementSelector('right-bracket-2'),
+    elementSelector('asterisk'),
+    elementSelector('rain-part-1'),
+    elementSelector('rain-part-2'),
+    elementSelector('rain-part-3'),
+    elementSelector('rain-part-4'),
   ], {
     opacity: 0,
     scale: 1,
   });
 }
-
 resetSprites();
 
 const mainTimeline = createTimeline({
@@ -33,15 +37,15 @@ const mainTimeline = createTimeline({
 
 mainTimeline
   .add([
-    "#canvas-art-1__left-bracket",
-    "#canvas-art-1__right-bracket",
+    elementSelector('left-bracket'),
+    elementSelector('right-bracket'),
   ], { // show brackets
     scale: [0.8, 1],
     opacity: [0, 1],
     duration: 600,
     ease: "outBack",
   })
-  .add("#canvas-art-1__asterisk", { // show asterisk
+  .add(CANVAS_SELECTOR + " [data-js-anim-el='asterisk']", { // show asterisk
     opacity: [0, 1],
     scale: [0.25, 1],
     rotate: [-60, 0],
@@ -49,82 +53,79 @@ mainTimeline
     ease: "outBack",
   }, "<<+=100")
   .add([
-    "#canvas-art-1__left-bracket",
-    "#canvas-art-1__right-bracket",
+    elementSelector('left-bracket'),
+    elementSelector('right-bracket'),
   ], { // morph to bigger brackets
     d: (_target, i, _l) =>
       document.querySelector(
         i === 0
-          ? "#canvas-art-1__left-bracket-2"
-          : "#canvas-art-1__right-bracket-2",
+          ? elementSelector('left-bracket-2')
+          : elementSelector('right-bracket-2'),
       )?.getAttribute("d") ?? "",
     duration: 600,
     ease: "inOutQuad",
   }, "+=500")
-
   .label("rain-start")
   .add([
-    "#canvas-art-1__rain-part-1",
-    "#canvas-art-1__rain-part-2",
-    "#canvas-art-1__rain-part-3",
-    "#canvas-art-1__rain-part-4",
+    elementSelector('rain-part-1'),
+    elementSelector('rain-part-2'),
+    elementSelector('rain-part-3'),
+    elementSelector('rain-part-4'),
   ], {
     y: { to: 45 }, // move rain parts down
-    duration: 3_200,
+    duration: 2_800,
     ease: "linear",
   })
-  .add("#canvas-art-1__rain-part-1", { // fade in rain part 1
+  .add(elementSelector('rain-part-1'), { // fade in rain part 1
     opacity: [0, 1],
     duration: 600,
     ease: "inQuad",
   }, "<<")
-  .add("#canvas-art-1__rain-part-2", { // fade in rain part 2
+  .add(elementSelector('rain-part-2'), { // fade in rain part 2
     opacity: [0, 1],
     duration: 600,
     ease: "inQuad",
   }, "<<+=200")
-  .add("#canvas-art-1__rain-part-3", { // fade in rain part 3
+  .add(elementSelector('rain-part-3'), { // fade in rain part 3
     opacity: [0, 1],
     duration: 600,
     ease: "inQuad",
-  }, "<<+=300")
-  .add('#canvas-art-1__rain-part-1', { // fade out rain part 1
+  }, "<<+=250")
+  .add(elementSelector('rain-part-1'), { // fade out rain part 1
     opacity: 0,
     duration: 300,
     ease: "outQuad",
   }, "rain-start+=600")
-  .add('#canvas-art-1__rain-part-2', { // fade out rain part 2
+  .add(elementSelector('rain-part-2'), { // fade out rain part 2
     opacity: 0,
     duration: 300,
     ease: "outQuad",
-  }, "rain-start+=1400")
-  .add('#canvas-art-1__rain-part-3', { // fade out rain part 3
+  }, "rain-start+=1200")
+  .add(elementSelector('rain-part-3'), { // fade out rain part 3
     opacity: 0,
     duration: 300,
     ease: "outQuad",
-  }, "rain-start+=2200")
+  }, "rain-start+=2000")
   .label("rain-end")
-  
-
+  .label("tree-grow-start")
+  .label("tree-grow-end", "+=600") // pause after rain
   .add([
-    "#canvas-art-1__left-bracket",
-    "#canvas-art-1__right-bracket",
+    elementSelector('left-bracket'),
+    elementSelector('right-bracket'),
   ], { // morph to smaller brackets
     d: (_target, i, _l) =>
       document.querySelector(
         i === 0
-          ? "#canvas-art-1__left-bracket-1"
-          : "#canvas-art-1__right-bracket-1",
+          ? elementSelector('left-bracket-1')
+          : elementSelector('right-bracket-1'),
       )?.getAttribute("d") ?? "",
     duration: 600,
     ease: "inOutQuad",
   }, "+=500")
-  .add("[data-js-canvas] path", { // fade out everything
+  .label("end-sequence-start")
+  .add(CANVAS_SELECTOR + " path", { // fade out everything
     opacity: 0,
     duration: 600,
     ease: "inQuad",
-  }, "+=2000")
-  .add("[data-js-canvas] path", {
-    duration: 1000, // 1 seconds
-    ease: "linear",
-  });
+  }, "+=1000")
+  .label("end-sequence-end");
