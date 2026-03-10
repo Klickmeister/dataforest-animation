26b50b7cc2dc1b373b8da6a93915efc6f5e66915
Finn Nils Maybauer <hi@finnmaybauer.me>
2026-03-10 15:21:40 +0100

    feat(animation): enhance vine animations and add light stream effects
    - Introduced new vine selectors and drawable SVG geometry for enhanced animation.
    - Implemented cloning of vine elements to create light stream effects.
    - Updated resetSprites function to initialize vine animations and light streams.
    - Modified mainTimeline to include new API formation animations with vine effects.
    - Removed unused tree animations and text animations for a cleaner sequence.
    - Adjusted timing and easing for smoother transitions in the animation flow.

--- src/animation-004.ts
+++ src/animation-004.ts
@@ -1,4 +1,4 @@
-import { createTimeline, spring, utils } from "animejs";
+import { createTimeline, spring, svg, utils, type DrawableSVGGeometry } from "animejs";
 
 const CANVAS_SELECTOR = '[data-js-canvas="animation-004"]';
 const elementSelector = (selector: string) => {
@@ -9,6 +9,40 @@ if (document.querySelector(CANVAS_SELECTOR) === null) {
   // don't run animation if canvas is not present
   throw new Error("Animation canvas not found");
 }
+
+const vineSelectors: string[] = [
+  elementSelector("vine-1"),
+  elementSelector("vine-2"),
+  elementSelector("vine-3"),
+  elementSelector("vine-4"),
+  elementSelector("vine-5"),
+  elementSelector("vine-6"),
+  elementSelector("vine-7"),
+  elementSelector("vine-8"),
+  elementSelector("vine-9"),
+  elementSelector("vine-10"),
+  elementSelector("vine-11"),
+  elementSelector("vine-12"),
+  elementSelector("vine-13"),
+];
+
+const vineSelectorsDrawable: DrawableSVGGeometry[][] = [];
+const vineLightStreamSelectors: string[] = [];
+
+const vinePointSelectors: string[] = [
+  elementSelector("vine-point-1"),
+  elementSelector("vine-point-2"),
+  elementSelector("vine-point-3"),
+  elementSelector("vine-point-4"),
+  elementSelector("vine-point-5"),
+  elementSelector("vine-point-6"),
+  elementSelector("vine-point-7"),
+  elementSelector("vine-point-8"),
+  elementSelector("vine-point-9"),
+  elementSelector("vine-point-10"),
+];
+
+const VINE_LIGHT_LENGTH = 28;
 
 function resetSprites() {
   utils.set([ // first frame setup
@@ -23,35 +57,60 @@ function resetSprites() {
     elementSelector("right-bracket-1"),
     elementSelector("left-bracket-2"),
     elementSelector("right-bracket-2"),
-    elementSelector("left-tree-trunk"),
-    elementSelector("left-tree-bottom"),
-    elementSelector("left-tree-top"),
     elementSelector("center-tree-trunk"),
     elementSelector("center-tree-bottom"),
     elementSelector("center-tree-mid"),
     elementSelector("center-tree-top"),
-    elementSelector("right-tree-trunk"),
-    elementSelector("right-tree-top"),
     elementSelector("logo"),
-    elementSelector("text-line-1"),
-    elementSelector("text-line-2"),
-    elementSelector("text-line-3"),
-    elementSelector("attack-left-1"),
-    elementSelector("attack-left-2"),
-    elementSelector("attack-left-3"),
-    elementSelector("attack-left-4"),
-    elementSelector("attack-left-5"),
-    elementSelector("attack-right-1"),
-    elementSelector("attack-right-2"),
-    elementSelector("attack-right-3"),
-    elementSelector("attack-right-3"),
-    elementSelector("attack-right-4"),
-    elementSelector("attack-right-5"),
-    elementSelector("attack-right-6"),
+    ...vinePointSelectors,
+    ...vineSelectors,
+    elementSelector("background-dots") + " path",
   ], {
     opacity: 0,
     scale: 1,
   });
+
+  // duplicate vines (if not already present)
+  vineSelectors.forEach((selector) => {
+    const element = document.querySelector(selector);
+
+    // Regex for adding a "-clone" suffix at the end of the elementSelector
+    const suffix = "-clone";
+
+    const cloneSelector = selector.replace(
+      /\[data-js-anim-el=['"]?([^'"[\]]+?)['"]?\]/,
+      `[data-js-anim-el='$1${suffix}']`,
+    );
+    const cloneDataAttr = cloneSelector.match(
+      /\[data-js-anim-el=['"]?([^'"[\]]+?)['"]?\]/,
+    )?.[1];
+
+    if (element && document.querySelector(cloneSelector) === null) {
+      const clone = element.cloneNode(true) as SVGPathElement;
+      clone.dataset.jsAnimEl = `${cloneDataAttr}`;
+      clone.classList.add("dataforest-animation-api__vine--light-stream");
+      element.parentNode?.appendChild(clone);
+      vineLightStreamSelectors.push(cloneSelector);
+    }
+  });
+
+  utils.set( // set up vines for draw animation
+    vineLightStreamSelectors,
+    {
+      strokeDasharray: (target, _i, _l) => {
+        const length = (target as SVGPathElement).getTotalLength();
+        return `${VINE_LIGHT_LENGTH} ${length}`;
+      },
+      strokeDashoffset: 0,
+      opacity: 0,
+      // strokeLinecap: "round",
+    },
+  );
+
+  vineSelectors.forEach((selector) => {
+    vineSelectorsDrawable.push(svg.createDrawable(selector));
+  });
+
 }
 resetSprites();
 
@@ -59,6 +118,8 @@ const easeSpring = spring({
   bounce: 0.34,
   duration: 400,
 });
+
+const randomSeed = utils.random(0, 1_000_000);
 
 const mainTimeline = createTimeline({
   autoplay: true,
@@ -79,7 +140,6 @@ mainTimeline
     duration: 600,
     ease: "outBack",
   })
-
   /**
    * Tree grow animation
    */
@@ -94,91 +154,73 @@ mainTimeline
           ? elementSelector("left-bracket-2")
           : elementSelector("right-bracket-2"),
       )?.getAttribute("d") ?? "",
-    x: (_target, i, _l) => (i === 0 ? -20 : 20),
     ease: easeSpring,
   })
-  .add([
-    elementSelector("left-tree-trunk"),
-    elementSelector("left-tree-bottom"),
-    elementSelector("left-tree-top"),
-  ], { // grow tree parts
-    opacity: [0, 1],
-    scale: [0.8, 1],
-    duration: 600,
-    delay: utils.stagger(150),
-    ease: "outBack",
-  }, "<<")
-  .add([
-    elementSelector("right-tree-trunk"),
-    elementSelector("right-tree-top"),
-  ], { // grow tree parts
-    opacity: [0, 1],
-    scale: [0.8, 1],
-    duration: 600,
-    delay: utils.stagger(150),
-    ease: "outBack",
-  }, "<<+=200")
   .label("trees-end")
   /**
-   * DDoS formation animation
+   * API formation animation
    */
-  .label("ddos-start")
-  .add(elementSelector("attack-left-1"), {
-    translateX: [-200, 0],
-    translateY: [-40, 0],
-    opacity: [1, 1],
+  .label("api-start")
+  .add([
+    elementSelector("background-dots") + " path",
+  ], {
+    opacity: [0, 1],
     duration: 250,
-    ease: "outQuad",
-  })
-  .add(elementSelector("attack-left-2"), {
-    translateX: [-200, 0],
-    translateY: [-40, 0],
-    opacity: [1, 1],
-    duration: 250,
-    ease: "outQuad",
-  }, '<<+=50')
-  .add(elementSelector("attack-left-3"), {
-    translateX: [-200, 0],
-    translateY: [-40, 0],
-    opacity: [1, 1],
-    duration: 250,
+    delay: () => utils.random(0, 500, 50),
     ease: "outQuad",
   }, '<<')
+  .add(vineSelectorsDrawable, {
+    draw: ["0 0", "0 1"],
+    opacity: [1],
+    duration: 600,
+    delay: () => utils.random(0, 500, 100),
+    easing: "inOutSine",
+  }, "-=200")
   .add([
-    elementSelector("attack-left-1"),
-    elementSelector("attack-left-2"),
-    elementSelector("attack-left-3"),
-    elementSelector("attack-left-4"),
-    elementSelector("attack-left-5"),
-    elementSelector("attack-right-1"),
-    elementSelector("attack-right-2"),
-    elementSelector("attack-right-3"),
-    elementSelector("attack-right-4"),
-    elementSelector("attack-right-5"),
-    elementSelector("attack-right-6"),
+    ...vinePointSelectors,
   ], {
-    scale: 1.5,
-    duration: 200,
+    opacity: [0, 1],
+    duration: 800,
+    easing: "easeInOutSine",
+  }, "<<")
+  .add(vineLightStreamSelectors, {
+    opacity: [0, 1],
+    duration: 250,
+    delay: utils.createSeededRandom(randomSeed, 0, 2_000),
+    ease: "outQuad",
+  }, "-=400")
+  .add(vineLightStreamSelectors, {
+    strokeDashoffset: (target, i, _l) => {
+      const length = (target as SVGPathElement).getTotalLength();
+
+      // odd vines go left, even vines go right
+      if (i % 2 === 0) {
+        return [length + VINE_LIGHT_LENGTH, VINE_LIGHT_LENGTH];
+      }
+
+      return [-length - VINE_LIGHT_LENGTH, VINE_LIGHT_LENGTH];
+    },
+    duration: 2000,
+    delay: utils.createSeededRandom(randomSeed, 0, 2_000),
     ease: "linear",
-  }, "<")
+  }, "<<-=10")
   .add([
-    elementSelector("attack-left-1"),
-    elementSelector("attack-left-2"),
-    elementSelector("attack-left-3"),
-    elementSelector("attack-left-4"),
-    elementSelector("attack-left-5"),
-    elementSelector("attack-right-1"),
-    elementSelector("attack-right-2"),
-    elementSelector("attack-right-3"),
-    elementSelector("attack-right-4"),
-    elementSelector("attack-right-5"),
-    elementSelector("attack-right-6"),
+    ...vinePointSelectors,
+    ...vineLightStreamSelectors,
+    elementSelector("background-dots") + " path",
   ], {
     opacity: 0,
-    duration: 100,
-    ease: "linear",
-  }, "<-=100")
-  .label("ddos-end")
+    duration: 250,
+    delay: () => utils.random(0, 500, 50),
+    ease: "inQuad",
+  })
+  .add(vineSelectorsDrawable, {
+    draw: ["0 1", "0 0"],
+    duration: 400,
+    delay: () => utils.random(0, 500, 50),
+    easing: "inOutSine",
+  }, "<<")
+  .label("api-end")
   /**
    * Logo formation animation
    */
@@ -207,7 +249,7 @@ mainTimeline
     opacity: 0,
     duration: 400,
     ease: "inOutQuad",
-  }, '<<')
+  }, "<<")
   .add([
     elementSelector("left-bracket"),
     elementSelector("right-bracket"),
@@ -233,43 +275,10 @@ mainTimeline
     ease: "inQuad",
   }, "+=2000")
   .label("end-sequence-end")
-  /**
-   * Text animations
-   */
-  .label("text-start")
-  .add(elementSelector("text-line-1"), {
-    opacity: [0, 1],
-    scale: [0.8, 1],
-    translateY: [2, 0],
-    duration: 150,
-    ease: easeSpring,
-  }, "trees-start+=50")
-  .add(elementSelector("text-line-2"), {
-    opacity: [0, 1],
-    scale: [0.8, 1],
-    translateY: [2, 0],
-    duration: 150,
-    ease: easeSpring,
-  }, "<<+=50")
-  .add(elementSelector("text-line-3"), {
-    opacity: [0, 1],
-    scale: [0.8, 1],
-    translateY: [2, 0],
-    duration: 150,
-    ease: easeSpring,
-  }, "trees-end")
-  .add([
-    elementSelector("text-line-1"),
-    elementSelector("text-line-2"),
-    elementSelector("text-line-3"),
-  ], {
-    opacity: { to: 0 },
-    translateY: 15,
-    duration: 150,
-    ease: "inQuad",
-  }, "logo-start")
   .add({}, {}, "+=2000") // pause before loop
 ;
+
+// mainTimeline.seek(2280)
 
 window.__CAPTURE__ = {
   duration: mainTimeline.duration,
